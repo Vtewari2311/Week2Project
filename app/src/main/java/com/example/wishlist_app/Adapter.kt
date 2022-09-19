496e55fff7c128260c0df0d224e9304f341371b6
Vtewari2311 <74680679+Vtewari2311@users.noreply.github.com>
2022-09-19 18:45:31 -0500

    Create Adapter.kt

---
+++ app/src/main/java/com/example/wishlist_app/Adapter.kt
@@ -0,0 +1,5 @@
+class ItemAdapter (private val mItems: List<Wish>) : RecyclerView.Adapter {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // Your holder should contain and initialize a member variable
+    }
+}
